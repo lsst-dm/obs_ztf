57eecd6b93ad2be36fe1764aab2afadb4023ea81
Robert Lupton the Good <rhl@astro.princeton.edu>
2019-07-27 08:06:46 -0500

    Fiddled configs to solve M31.  Noise is low??

--- config/processCcd.py
+++ config/processCcd.py
@@ -21,6 +21,10 @@ for loader in [config.calibrate.astromRefObjLoader, config.calibrate.photoRefObj
 config.charImage.repair.cosmicray.nCrPixelMax = 1000000
 config.charImage.repair.interp.modelPsf.defaultFwhm = 0.1
 
+config.isr.fwhm=1.00
+config.charImage.installSimplePsf.fwhm=1.01
+config.charImage.repair.interp.modelPsf.defaultFwhm=1.02
+
 # PSF determination
 config.charImage.measurePsf.reserve.fraction = 0.2
 config.charImage.measurePsf.starSelector["objectSize"].widthMin = 0.9
@@ -35,3 +39,17 @@ try:
 except ImportError as e:
     print("WARNING: Unable to use psfex: %s" % e)
     config.charImage.measurePsf.psfDeterminer.name = "pca"
+
+config.charImage.detection.thresholdValue=70
+
+config.calibrate.detection.thresholdValue=80
+config.calibrate.astrometry.referenceSelector.doSignalToNoise=False # all errors are NaN
+config.calibrate.astrometry.referenceSelector.signalToNoise.minimum=10
+config.calibrate.astrometry.referenceSelector.signalToNoise.fluxField='phot_g_mean_mag_flux'
+config.calibrate.astrometry.referenceSelector.signalToNoise.errField='phot_g_mean_mag_fluxErr'
+
+config.calibrate.astrometry.referenceSelector.doMagLimit=True
+config.calibrate.astrometry.referenceSelector.magLimit.fluxField='phot_g_mean_mag_flux'
+config.calibrate.astrometry.referenceSelector.magLimit.maximum = 16.5 # AB
+
+config.calibrate.astrometry.wcsFitter.order = 5
